ce11f6287e74104fd5c6a195e8045c24ea4622b4
stanislas <cisternas@et.esiea.fr>
2022-11-18 19:03:11 +0100

    V6: Tournament

--- towerAI.py
+++ towerAI.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process, freeze_support
 import multiprocessing
+from multiprocessing.sharedctypes import Value
 from time import sleep
 from sys import exit
 import keyboard
@@ -12,6 +13,18 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 import cv2 as cv
 import numpy as np
+
+from datetime import datetime
+from time import strftime
+
+
+def wfile(msg, mode='a'):
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    with open('logs.txt', mode) as f:
+        txt = "{}: {}\n".format(dt_string, msg)
+        f.write(txt)
 
 class TowerAI:
     
@@ -27,10 +40,13 @@ class TowerAI:
 
     # On Phase 2, we focus on the Value in the OCRed string. 
     # On Phase 3, we focus on the Price in the OCRed string.
-    __focusValue__ = True     
+    __focusValue__ = True    
+
+    __nphase__ =  0
 
     # This section gets the main monitor's features (x and y start position, width and height).
     def __init__(self):
+        wfile("\n\n\t\tInitialisation", 'w')
         monitor = get_monitors()[0]
         self.main_screen = (monitor.x, monitor.y, monitor.width, monitor.height)
         #print(main_screen)
@@ -44,18 +60,27 @@ class TowerAI:
         X_retry = self.main_screen[0] + 771
         Y_retry = 783
 
+        X_diamond = self.main_screen[0] + 686
+        Y_diamond = 395
+
+        X_tournament = self.main_screen[0] + 877
+        Y_tournament = 609
+
+        X_start = self.main_screen[0] + 822
+        Y_start = 772      
+
         stride = 5
 
         # loc variables contain starting X and Y, then Width and Height of the box.
         self.__locTL__ = (X_stat_box + stat_box_width / 2, Y_stat_box + 30, stat_box_width / 2 - 15, stat_box_height - 45)
         # center variables contains the center of the boxes, to click later when needed.
-        self.__centerTL__ = (X_stat_box + stat_box_width / 2, Y_stat_box + stat_box_height / 2)
+        self.__centerTL__ = (X_stat_box + stat_box_width / 2 + 50, Y_stat_box + stat_box_height / 2)
 
         self.__locTR__ = (X_stat_box + stat_box_width + stride + stat_box_width / 2, Y_stat_box + 29, stat_box_width / 2 - 15, stat_box_height - 45)
-        self.__centerTR__ = (X_stat_box + stat_box_width + stride + stat_box_width / 2, Y_stat_box + stat_box_height / 2) 
-
+        self.__centerTR__ = (X_stat_box + stat_box_width + stride + stat_box_width / 2 + 50, Y_stat_box + stat_box_height / 2) 
+ 
         self.__locBR__ = (X_stat_box + stat_box_width + stride + stat_box_width / 2, Y_stat_box + stat_box_height + 35, stat_box_width / 2 - 40, stat_box_height - 45)
-        self.__centerBR__ = (X_stat_box + stat_box_width + stride + stat_box_width / 2, Y_stat_box + stat_box_height + stat_box_height / 2)
+        self.__centerBR__ = (X_stat_box + stat_box_width + stride + stat_box_width / 2 +  stat_box_width / 3, Y_stat_box + stat_box_height + stat_box_height / 2)
 
         # Need to find the Sword, Shield, Star position and centers. Same proccess than before.
         X_tab_box = self.main_screen[0] + 1357 / 2
@@ -73,7 +98,17 @@ class TowerAI:
         self.__locRetry__ = (X_retry, Y_retry, 173, 90)
         self.__centerRetry__ = (X_retry + 87, Y_retry + 45)
 
+        self.__locDiamond__ = (X_diamond, Y_diamond, 132, 81)
+        self.__centerDiamond__ = (X_diamond + 132 / 2, Y_diamond + 81 / 2)
+
+        self.__locTournament__ = (X_tournament, Y_tournament, 165, 86)
+        self.__centerTournament__ = (X_tournament + 165 / 2, Y_tournament + 86 / 2)
+
+        self.__locStart__ = (X_start, Y_start, 280, 112)
+        self.__centerStart__ = (X_start + 280 / 2, Y_start + 112 / 2)      
+
         self.exit = multiprocessing.Event()
+        self.pause = multiprocessing.Event()
 
 ##### PROCESSING METHODS #####
     def __click__(self, item):
@@ -88,28 +123,34 @@ class TowerAI:
 
         return pytesseract.image_to_string(threshed_screen)    
 
-    def __letterToNumber__(self, x):  
+    def __letterToNumber__(self, x):          
 
         if x == '' :
-            print("Problème")
+            wfile("__letterToNumber__ returns -1")            
             return -1
 
-        if 'K' in x:
-            return int(round(float(x.replace('K', '')) * 1000))
+        try:
 
-        if 'M' in x:       
-            return int(round(float(x.replace('M', '')) * 1000000))
+            if 'K' in x:
+                return int(round(float(x.replace('K', '')) * 1000))
+
+            if 'M' in x:       
+                return int(round(float(x.replace('M', '')) * 1000000))
+
+        except ValueError:
+            return -1
 
         return int(round(float(x)))
 
-    def __processString__(self, x):          
-        x = x.split("\n")
-        
-        # print(x)
-        
+    def __processString__(self, x):   
 
-        if(x[0][0] in ['x']):
-            x = x[1:]                    
+        x = x.split("\n")              
+        x.remove('') 
+        
+        wfile("In processingString: x = {}".format(x))     
+
+        if(x == []):
+            return x                   
     
         x = [i.replace(',', '.') for i in x]
         x = [i.replace('$ ', '') for i in x]
@@ -117,7 +158,10 @@ class TowerAI:
         # If Shield, returns value (x[0]), else returns price (x[1])
         return x[0] if self.__focusValue__ else x[1]
 
-    def __getPvPrice__(self):
+    def __getPvPrice__(self):      
+
+        wfile("In getPvPrice") 
+
         result_ocr = self.__screenToString__(region=self.__locTL__)  
         
         x = result_ocr.split("\n")
@@ -126,11 +170,15 @@ class TowerAI:
             x = x[1:]  
 
         x = [i.replace(',', '.') for i in x]
-        x = [i.replace('$ ', '') for i in x]                
+        x = [i.replace('$ ', '') for i in x]   
+
+        wfile("x after processing: {}".format(x))
+
+        result = self.__letterToNumber__(x[1])             
             
-        return self.__letterToNumber__(x[1])  
+        return result
 
-
+ 
  ##### TEST METHODS #####       
 
     def test_ocr(self):      
@@ -179,122 +227,209 @@ class TowerAI:
     def __runGame__(self):           
 
         while not self.exit.is_set():
+            if not self.pause.is_set():
+                if self.__nphase__ != 2:
+                    self.__click__(self.__centerShield__)
+                try:
+                    self.__phase2__()
+                except ValueError:
+                    continue
 
-            self.__phase2__()
-            self.__phase3__()
+                self.__click__(self.__centerSword__)
+                try:
+                    self.__phase3__()
+                except ValueError:
+                    continue                          
 
-            self.__updateThreshs__()           
-
+                self.__updateThreshs__() 
+                       
     def __phase2__(self):
-        self.__focusValue__ = True
-        self.__click__(self.__centerShield__)
+        wfile("\tIn __phase2__")
+        self.__focusValue__ = True      
+        self.__nphase__ = 2 
 
         regen = -1
 
         while(regen < self.__regenThresh__ and not self.exit.is_set()):
-            sleep(0.5)
-            self.__click__(self.__centerTR__)
-            result_ocr = self.__screenToString__(region=self.__locTR__)  
-            if result_ocr == ['']:
-                continue
-            processed_result = self.__processString__(result_ocr)        
-            regen = self.__letterToNumber__(processed_result)
-            print("Regen: ", regen)
+            if not self.pause.is_set():
+                sleep(0.5)
+                self.__click__(self.__centerTR__)
+                result_ocr = self.__screenToString__(region=self.__locTR__)  
+            
+                processed_result = self.__processString__(result_ocr) 
+                if processed_result == []:
+                    wfile("No match")   
+                    continue    
+                regen = self.__letterToNumber__(processed_result)
+                wfile("Regen: {}".format(regen))
 
         defense = -1
         
         while(defense < self.__defenseThresh__ and not self.exit.is_set()):
-            sleep(0.5)
-            self.__click__(self.__centerBR__)
-            result_ocr = self.__screenToString__(region=self.__locBR__)
-            if result_ocr == ['']:
-                print("OK")
-                continue        
-            processed_result = self.__processString__(result_ocr)        
-            defense = self.__letterToNumber__(processed_result)
-            print("Defense: ", defense)
+            if not self.pause.is_set():
+                sleep(0.5)
+                self.__click__(self.__centerBR__)
+                result_ocr = self.__screenToString__(region=self.__locBR__)
+                    
+                processed_result = self.__processString__(result_ocr)    
+                if processed_result == []:
+                    wfile("No match")   
+                    continue     
+                defense = self.__letterToNumber__(processed_result)
+                wfile("Defense: {}".format(defense))
 
         pv = -1
 
         while(pv < self.__pvThresh__ and not self.exit.is_set()):
-            sleep(0.5)
-            self.__click__(self.__centerTL__)
-            result_ocr = self.__screenToString__(region=self.__locTL__)      
-            if result_ocr == ['']:
-                continue  
-            processed_result = self.__processString__(result_ocr)        
-            pv = self.__letterToNumber__(processed_result)
-            print("pv: ", pv)
+            if not self.pause.is_set():
+                sleep(0.5)
+                self.__click__(self.__centerTL__)
+                result_ocr = self.__screenToString__(region=self.__locTL__)      
+                
+                processed_result = self.__processString__(result_ocr)   
+                if processed_result == []:
+                    wfile("No match")   
+                    continue      
+                pv = self.__letterToNumber__(processed_result)
+                wfile("pv: {}".format(pv))
 
         # Need to check pv price here and store the value.
         self.__pvPrice__ = self.__getPvPrice__()
         
-        print("End of Phase 2.")
+        wfile("End of Phase 2.")
 
     def __phase3__(self):
-        self.__focusValue__ = False
-        self.__click__(self.__centerSword__)
+        wfile("\tIn __phase3__")
+        self.__focusValue__ = False 
+        self.__nphase__ = 3        
 
         attaquePrice = -1
 
-        while(attaquePrice < self.__pvPrice__ and not self.exit.is_set()):
-            sleep(0.5)
-            self.__click__(self.__centerTL__)
-            result_ocr = self.__screenToString__(region=self.__locTL__)    
-            if result_ocr == ['']:
-                continue    
-            processed_result = self.__processString__(result_ocr)        
-            attaquePrice = self.__letterToNumber__(processed_result)
+        wfile("attaquePrice = {}".format(attaquePrice))
+        wfile("pvPrice = {}".format(self.__pvPrice__))
+        wfile("self.exit.is_set() = {}".format(self.exit.is_set()))
+        
+        while(attaquePrice < self.__pvPrice__ and not self.exit.is_set()):   
+            if not self.pause.is_set():        
+                sleep(0.5)
+                self.__click__(self.__centerTL__)
+                result_ocr = self.__screenToString__(region=self.__locTL__)    
+                
+                processed_result = self.__processString__(result_ocr)    
+                if processed_result == []:
+                        wfile("No match")   
+                        continue     
+                attaquePrice = self.__letterToNumber__(processed_result)
 
-        print("End of Phase 3.")
+        wfile("End of Phase 3.")
 
-    def __updateThreshs__(self):
+    def __updateThreshs__(self): 
         self.__regenThresh__ += self.__regenIncr__
         self.__defenseThresh__ += self.__defenseIncr__
         self.__pvThresh__ += self.__pvIncr__
 
+    def __searchDiamonds__(self):
+
+        wfile("---------------------In searchDiamonds---------------------")
+
+        while not self.exit.is_set():
+            
+            diamond = pyautogui.locateOnScreen('./images/DiamondPopUp.png', region=self.__locDiamond__)
+
+            if diamond != None:            
+                wfile("\t\t\tDIAMOND FOUND !!")
+                print("DIAMOND FOUND !! :)")
+                
+                # TODO: Need to pause the runGame process here
+                self.pause.set()
+                self.__click__(self.__centerDiamond__)
+                # TODO: Unpause the runGame process here
+                self.pause.clear()
+                sleep(5)
+
+    def __get_tournament__(self):
+
+        wfile("---------------------In getTournament---------------------")
+
+        while not self.exit.is_set():
+            year, month, day, hour, min = map(int, strftime("%Y %m %d %H %M").split())
+
+            if(hour == 2 and min <= 10):
+                wfile("IT'S 2AM !!!")
+
+                tournamentScreen = pyautogui.locateOnScreen('./images/Tournament.png', region=self.main_screen)           
+                        
+                if(tournamentScreen != None):                      
+                    self.pause.set()     
+                    self.__click__(self.__centerTournament__)
+                    sleep(5)
+                    self.__click__(self.__centerStart__)
+                    self.pause.clear()                
+
     def appManager(self):
       
         while not self.exit.is_set():
-            # In this part, we launch to game thread.
-            print("\n\n/!\ Debut du jeu ! /!\ \n\n")
             runGame = Process(target=self.__runGame__)
-            runGame.start()
-            # In this part, we have to search if the ending screen is displayed. 
-            # If yes, then kill the runGame thread.        
-            # Then click on Retry and start a new runGame thread.
-            while not self.exit.is_set():                              
-                endingscreen = pyautogui.locateOnScreen('./images/Retry.png', region=self.main_screen)           
+            searchDiamond = Process(target=self.__searchDiamonds__)
+            tournament = Process(target=self.__get_tournament__)
+            try:
+                # In this part, we launch to game thread.
+                wfile("\n\n/!\ Debut du jeu ! /!\ \n\n")
                 
-                if(endingscreen != None):                    
-                    runGame.kill()
-                    print("\n\n/!\ Fin du jeu ! /!\ \n\n")
-                    sleep(5)
-                    self.__click__(self.__centerRetry__)
-                    break
+                runGame.start()
+                searchDiamond.start()
+                tournament.start()
+                # In this part, we have to search if the ending screen is displayed. 
+                # If yes, then kill the runGame thread.        
+                # Then click on Retry and start a new runGame thread.
+                if not self.pause.is_set():
+                    while not self.exit.is_set():                              
+                        endingscreen = pyautogui.locateOnScreen('./images/Retry.png', region=self.main_screen)           
+                        
+                        if(endingscreen != None):        
+                            searchDiamond.kill()            
+                            runGame.kill()
+
+                            wfile("\n\n/!\ Fin du jeu ! /!\ \n\n")
+                            sleep(5)
+                            self.__click__(self.__centerRetry__)
+                            break
+            except ValueError:
+                wfile("HUGO DIS MOI SI TU VOIS CE MESSAGE !!!!!!")
+                searchDiamond.kill()
+                runGame.kill()
+                continue 
 
     def quit_app(self):
-        while(not self.exit.is_set()):           
+        while not self.exit.is_set():           
             if keyboard.is_pressed("space"):
                 print("Shutting Down the app")
+                wfile("Shutting Down the app")
                 self.exit.set()
-                exit()            
+                exit()         
+
+ 
+               
+
 
 def main():
-   
+      
     if __name__== "__main__" :
+         
         freeze_support()  
 
-        print("Press Space to start the AI.")
-
+        print("Press Space to start the  AI.") 
+ 
         while not keyboard.is_pressed("space"):
-            pass 
+            pass  
 
-        tower = TowerAI()
+        tower = TowerAI() 
+        
         Process(target=tower.quit_app).start()
+        
         
         tower.appManager()     
        
         print("Fin du programme")     
       
-main()
+main() 
